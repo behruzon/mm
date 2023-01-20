d126676f269b5b32e1016f7d4abffc921834dc94
behruzon <113119434+behruzon@users.noreply.github.com>
2023-01-20 16:56:23 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let doc = document
+
+
+
+let a = doc.querySelector('.div1')
+let b = doc.querySelector('.div2')
+let v = doc.querySelector('.div3')
+let img = doc.querySelector('img')
+
+
+b.onclick = ()=>{
+    img.src='img/image 9.png'
+
+}
+
+v.onclick = ()=>{
+    img.src='img/image 8.png'
+
+}
+a.onclick = ()=>{
+    img.src='img/image 1 (11).png'
+
+}
